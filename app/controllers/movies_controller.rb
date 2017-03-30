ca906b6ba04a943f321847c9f0078ea5fdfd6a86
cwy007 <chanweiyan007@gmail.com>
2017-03-30 14:29:22 +0800

    implement movies#edit and movies#create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,5 +2,24 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
-  
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path, notice: "Movie create successfully!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description)
+  end
 end
